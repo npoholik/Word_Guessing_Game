6029c5bd0a7f647ae5101d6e3c996b1a3d50836a
npoholik <nick.poholik@slu.edu>
2023-12-04 22:27:59 -0600

    Rough Draft Version

--- Main.py
+++ Main.py
@@ -4,5 +4,16 @@ from SecretWord import *
 
 test = SecretWord()
 
+while(True):
+    print("Guess a character: ") 
+    guess = input()
+
+    if guess:
+        test.guess(guess[0])
+    else:
+        print("Invalid Input")
+    
+    break
+
 
 
--- SecretWord.py
+++ SecretWord.py
@@ -1,7 +1,12 @@
 import random
 
 class SecretWord:
+    __turns = 0
+    __word = ""
+    __lost = 0
+    __won = 0
 
+    #Class constructor
     def __init__(self):
         file=open("Dictionary.txt","r") # Read in files from text file
         file.seek(0)
@@ -9,9 +14,28 @@ class SecretWord:
     
         selection = random.randint(0, len(self.word_list)-1) # Obtain a random index
         self.word = self.word_list[selection]  #Store word from random index
-
-        print(self.word)
-
+        self.turns = 8
 
 
+    # Method to handle a user guess
+    def guess(self, user_guess):
+        self.turns -= 1
+        for char in self.word: 
+            if char in self.word:
+                print(char, end=" ")
+            else:
+                print(" _ ")
+                self.turns -= 1
+                if self.turns == 0:
+                    self.lost = 1
+        
+        
 
+    def getTurns(self):
+        return self.turns
+    
+    def hasWon(self):
+        return self.won
+    
+    def hasLost(self):
+        return self.lost
